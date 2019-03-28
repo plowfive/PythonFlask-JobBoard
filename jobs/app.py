f6b6f9381d62907fe08e573769050dc29a31d8d1
Ken Plowman <kenplowman@gmail.com>
2019-03-28 21:35:46 +1100

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -12,22 +12,22 @@ def open_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
-    def execute_sql(sql, values=(), commit=False, single=False):
-        connection = open_connection()
-        cursor = connection.execute(sql, values)
-        if commit == True:
-            results = connection.commit()
-        else:
-            results = cursor.fetchone() if single else cursor.fetchall()
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
 
-        cursor.close()
-        return results
+    cursor.close()
+    return results
 
-    @app.teardown_appcontext
-    def close_connection(exception):
-        connection = getattr(g, '_connection', None)
-        if connection is not None:
-            connection.close()
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
 
 
